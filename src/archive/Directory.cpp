e52fc2e037e8fd1c30cdf868b3c00cc46b25f4ec
Ahmed Moataz <ahmedmo3taz111@gmail.com>
2025-12-14 17:11:57 +0200

    Fix formatting and comments in Directory.cpp

--- src/archive/Directory.cpp
+++ src/archive/Directory.cpp
@@ -9,9 +9,9 @@ std::vector<std::string> DirectoryCrawler::getAllFiles(const std::string& rootPa
 
         // Loop through directory recursively
 
-        for (const auto& entry : fs::recursive_directory_iterator(rootPath)) {    //ميثود جاهزةوبطبعها recursive وهتاخد الباث المبعوت ليها منا اللي بيظبط ال main
-            if (entry.is_regular_file()) {   //لو لقي فايل (مش فولدر او شورتكات
-                files.push_back(entry.path().generic_string());  //هيضيفه للvector بتاعي علي هيئة path (to string->totally)
+        for (const auto& entry : fs::recursive_directory_iterator(rootPath)) { 
+            if (entry.is_regular_file()) { 
+                files.push_back(entry.path().generic_string()); 
             }
         }
 
